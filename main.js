5f87231c51feee5eee2cb4dc5570f8c6fc6d9165
Douglas Flores <Dflores5df@icloud.com>
2023-04-29 20:21:01 -0400

    lab done

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const likeButtons = document.querySelectorAll(".like-glyph")
+likeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    mimicServerCall()
+      .then(resp => {
+        button.classList.add('activated-heart')
+        button.textContent = FULL_HEART
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        const errorDom = document.createElement('p')
+        errorDom.textContent = ` ${errorMessage}`
+        document.body.appendChild(errorDom)
+        document.querySelector('#modal').classList.remove('hidden');
+        setTimeout(() => {
+          document.querySelector('#modal').classList.add('hidden');
+        }, 3000);
+      })
+  })
+  
+  button.addEventListener('click', () => {
+    if (button.classList.contains('activated-heart')) {
+      button.classList.remove('activated-heart');
+      button.textContent = EMPTY_HEART;
+    }
+  });
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
